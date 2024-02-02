d16dfd25fa8f73e9dbd34daa08bc20fc20bf2af5
kenny <kenny910424@gmail.com>
2024-02-02 20:21:04 +0800

    login route finish and add some check in register route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,22 @@
 const router = require("express").Router();
 const registerValidation = require("../validation").registerValidation;
+const loginValidation = require("../validation").loginValidation;
 const User = require("../models").user
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const saltRounds = 10;
 
 router.post("/register", async (req, res) => {
     const data = req.body;
     const { error } = registerValidation(data);
     if (error) {
-        return res.status(400).send(error);
+        return res.status(400).send(error.details[0].message);
+    }
+    let emailExist = await User.findOne({ email: data.email }).exec();
+    if (emailExist) {
+        return res.status(400).send("email already exist");
     }
     const hashedPassword = bcrypt.hashSync(data.password, saltRounds);
     const newUser = new User({
@@ -23,6 +31,24 @@ router.post("/register", async (req, res) => {
     } catch (e) {
         return res.status(500).send(e);
     }
+})
+
+router.post("/login", async (req, res) => {
+    const data = req.body;
+    const { error } = loginValidation(data);
+    if (error) {
+        return res.status(400).send(error);
+    }
+    let loginUser = await User.findOne({ email: data.email }).exec();
+    if (!loginUser) {
+      return res.status(400).send("user doesn't exist");
+    }
+    if (!bcrypt.compareSync(data.password, loginUser.password)) {
+        return res.status(400).send("password not correct");
+    }
+    const tokenObject = { _id: loginUser._id, email: loginUser.email };
+    const token = jwt.sign(tokenObject, process.env.PASSPORT_SECRET);
+    return res.send({ msg: "login success", user: loginUser, token: "JWT "+token });
 })
 
 module.exports = router;
--- server/validation.js
+++ server/validation.js
@@ -10,4 +10,13 @@ const registerValidation = (data) => {
     return schema.validate(data);
 }
 
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+  });
+  return schema.validate(data);
+}
+
 module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
