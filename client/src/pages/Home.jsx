dcc8fd8541a64b51baacedfcf096bcbf60dbda4f
kenny <kenny910424@gmail.com>
2024-02-18 21:46:43 +0800

    delete buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
             作為後端服務器。這種項目稱為 MERN
             項目，它是創建現代網站的最流行的方式之一。
           </p>
-          <button className="btn btn-primary">看看他怎麼運作</button>
         </div>
       </div>
       <div className="row">
@@ -21,7 +20,6 @@ const Home = () => {
             <p className="col-md-8 text-light">
               學生可以註冊他們喜歡的課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
             </p>
-            <button className="btn btn-outline-light">登入會員，或者註冊一個帳號</button>
           </div>
         </div>
         
@@ -31,15 +29,10 @@ const Home = () => {
             <p className="col-md-8">
               您可以通過註冊成為一名講師，並開始製作在線課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
             </p>
-            <button className="btn btn-outline-dark">開始開設課程</button>
           </div>
         </div>
-        
       </div>
-
     </div>
-    
-    
   );
 };
 
